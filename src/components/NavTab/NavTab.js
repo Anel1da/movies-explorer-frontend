58c50e1a3dbbc2d91015c8411f434f53c3ecb671
Lidya <anel1da@yandex.ru>
2021-08-27 14:29:25 +0300

    доработанa высота блока promo

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -6,17 +6,17 @@ export default function NavTab() {
   return (
     <nav className="navtab">
       <ul className="navtab__navigation">
-        <li>
+        <li className="navtab__link-item">
           <a href="#about" className="navtab__link">
             О проекте
           </a>
         </li>
-        <li>
+        <li className="navtab__link-item">
           <a href="#tech" className="navtab__link">
             Технологии
           </a>
         </li>
-        <li>
+        <li className="navtab__link-item">
           <a href="#student" className="navtab__link">
             Студент
           </a>
